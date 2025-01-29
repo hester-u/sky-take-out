6b271925ef343966678fe5fb839589730ec79ede
hester-u <934411970@qq.com>
2025-01-29 21:00:39 +0800

    echart订单菜品销售排行

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -69,4 +70,12 @@ public interface OrderMapper {
      * @param map
      */
     Double sumByMap(Map map);
+
+    Integer countByMap(Map map);
+    /**
+     * 查询商品销量排名
+     * @param begin
+     * @param end
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
